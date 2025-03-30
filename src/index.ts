67aa13a4673aa4c2b70de1521745dd346019cb54
Thamarai <tmanalan710@gmail.com>
2025-03-30 19:12:08 +0530

    Commited

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import data from "./data"
+import cors from 'cors'
 const app = express()
-
+app.use(cors())
 app.get('/',(req,res)=>{
       res.json(data)
 })
 
 app.listen(process.env.PORT || 3000)
+
+
